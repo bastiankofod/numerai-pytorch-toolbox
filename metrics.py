e315f7ea975be728a7f406e7627cc1e5432b8dfb
bastiankofod <bastiankofod@live.dk>
2020-12-14 13:31:25 +0100

    Added more metrics and real world test data

--- metrics.py
+++ metrics.py
@@ -1,13 +1,17 @@
+"""
+Thanks to http://www.turingfinance.com/computational-investing-with-python-week-one/ for many of the metrics.
+"""
+
 import torch
+
 
 def sharpe(x):
     return x.mean() / x.std()
 
+
 def numerai_sharpe(x):
     return (x.mean() - 0.010415154) / x.std()
 
-def ar1(x):
-    return 
 
 def skew(x):
     mx = x.mean()
@@ -15,15 +19,58 @@ def skew(x):
     m3 = ((x-mx)**3).mean()
     return m3 / (m2**1.5)
 
+
 def kurtosis(x):
     mx = x.mean()
     m2 = ((x-mx)**2).mean()
     m4 = ((x-mx)**4).mean()
     return (m4 / (m2**2)) - 3
 
+
 def adj_sharpe(x, sharpe_f):
     return sharpe_f(x) * (1 + ((skew(x) / 6) * sharpe_f(x)) - ((kurtosis(x) / 24) * (sharpe_f(x)**2)))
 
+
+def lpm(r, t, o):
+    rt = t - r
+    return torch.sum(torch.minimum(torch.tensor(1e-7), rt) ** o) / len(rt)
+
+
+def hpm(r, t, o):
+    rt = r - t
+    return torch.sum(torch.minimum(torch.tensor(1e-7), rt) ** o) / len(rt)
+
+
 def sortino(x, t=0.010415154):
+    return x.mean() / torch.sqrt(lpm(x, t, 2))
+
+
+def numerai_sortino(x, t=0.010415154):
     xt = x - t
-    return xt.mean() / (torch.sum(torch.minimum(torch.tensor(1e-7), xt)**2)/(len(xt)-1))**0.5
+    return xt.mean() / torch.sqrt(lpm(x, t, 2))
+
+
+def kappa_three(x, t=0.010415154):
+    l = lpm(x, t, 3)
+    return x.mean() / torch.pow(torch.abs(l), float(1/3))
+
+
+def gain_loss_ratio(x, t=0.010415154):
+    return hpm(x, t, 1) / lpm(x, t, 1)
+
+
+def upside_potential_ratio(x, t=0.010415154):
+    return hpm(x, t, 1) / torch.sqrt(lpm(x, t, 2))
+
+
+# TODO: max_dd doesn't return the same result as Numerai's max drawdown.
+#  This is due to how pandas does rolling windows.
+#  To be resolved!
+def max_dd(x, w=20):
+    r = torch.max(torch.cumprod(x+1, dim=0).unfold(0, w, 1))
+    d = torch.cumprod(x+1, dim=0)
+    return -torch.max(r - d)
+
+
+def calmar(x):
+    return x.mean() / max_dd(x)
--- test_metrics.py
+++ test_metrics.py
@@ -1,7 +1,9 @@
-import torch
 from metrics import *
 
-x = torch.randn(100)
+x = torch.FloatTensor([0.0308,  0.0240,  0.0360,  0.0314,  0.0033,  0.0097,  0.0221,  0.0263,
+                       -0.0083,  0.0329,  0.0182,  0.0251,  0.0272,  0.0447,  0.0099, -0.0234,
+                       -0.0007,  0.0217,  0.0123,  0.0175,  0.0303,  0.0011,  0.0175,  0.0377,
+                       0.0149, -0.0320,  0.0150,  0.0232])
 
 print('##### TESTING METRICS! #####')
 print(f'Sharpe ratio is: {sharpe(x)}')
@@ -11,4 +13,9 @@ print(f'Kurtosis is: {kurtosis(x)}')
 print(f'Adjusted sharpe ratio is: {adj_sharpe(x, sharpe)}')
 print(f'Adjusted numerai sharpe ratio is: {adj_sharpe(x, numerai_sharpe)}')
 print(f'Sortino ratio is: {sortino(x)}')
-
+print(f'Numerai sortino ratio is: {numerai_sortino(x)}')
+print(f'Kappa three ratio is: {kappa_three(x)}')
+print(f'Gain/Loss ratio is: {gain_loss_ratio(x)}')
+print(f'Upside potential ratio is: {upside_potential_ratio(x)}')
+print(f'Max drawdown is: {max_dd(x, 5)}')
+print(f'Calmar ratio is: {calmar(x)}')
